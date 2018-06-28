9c1476be1b6946ef7bbc7c60927e6b15f76d9d29
rkreja <rkrejanyc@yahoo.com>
2018-06-28 00:28:09 -0400

    test update code commit

--- MyWebApplication/src/net/timeandtraining/base/Test.java
+++ MyWebApplication/src/net/timeandtraining/base/Test.java
@@ -5,7 +5,7 @@ public class Test {
 	public static void main(String[] args) {
 		
 		
-		Base.getDriver().get("http://www.cnn.com");		
+		Base.getDriver().get("http://www.timeandtraining.net");		
 
 	}
 
